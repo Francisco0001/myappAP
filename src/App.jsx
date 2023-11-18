0cfe19e870330fdcdd395fe5847f5e398d0b1eaa
Francisco0001 <frangonza7@gmail.com>
2023-11-17 22:19:34 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import DetallesProducto from './components/DetallesProducto/DetallesProducto'
+
+function App() {
+  return (
+    <>
+      <DetallesProducto titulo='Pileta 3x60 x 1x30' precio='400' sku='UJS7F733J' stock='15' descripcion='lorem impusm lorem impusm lorem impusm lorem impusm'/>
+    </>
+  )
+}
+
+export default App
